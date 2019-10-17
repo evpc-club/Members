256ef9d8cf9a98408f56fef54f8a97e49b6e24bb
NguyenEvan <55529290+NguyenEvan@users.noreply.github.com>
2019-10-16 20:07:46 -0700

    Create script.js

---
+++ Personal Projects/Evan Nguyen/script.js
@@ -0,0 +1,17 @@
+function insert(val)
+{
+    document.form.display.value = document.form.display.value + val;
+}
+
+function clearDisplay()
+{
+
+}
+function evaluateExpression()
+{
+
+}
+function deletefromEnd()
+{
+  
+}
